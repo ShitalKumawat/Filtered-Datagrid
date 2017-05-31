89530c610df9727ad524e477908bf6f42bd04b42
SHIASH <SHIASH@SHITALASHIM>
2017-05-31 15:34:45 +0530

    Updated the common library reference

---
+++ Common/ReflectionHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class ReflectionHelper
+    {
+        public static Object GetPropertyValue(this Object obj, String propertyName)
+        {
+            string[] nameParts = propertyName.Split('.');
+            if (nameParts.Length == 1)
+            {
+                return obj.GetType().GetProperty(propertyName).GetValue(obj, null);
+            }
+
+            foreach (String part in nameParts)
+            {
+                if (obj == null) { return null; }
+
+                Type type = obj.GetType();
+                PropertyInfo info = type.GetProperty(part);
+                if (info == null) { return null; }
+
+                obj = info.GetValue(obj, null);
+            }
+            return obj;
+        }
+
+        public static Type GetPropertyType(String name, Type type)
+        {
+            var parts = name.Split('.').ToList();
+            var currentPart = parts[0];
+            PropertyInfo info = type.GetProperty(currentPart);
+            if (info == null) { return null; }
+            if (name.IndexOf(".", StringComparison.Ordinal) > -1)
+            {
+                parts.Remove(currentPart);
+                return GetPropertyType(String.Join(".", parts), info.PropertyType);
+            }
+            return info.PropertyType;
+        }
+    }
+}
---
+++ Common/TypeSwitch.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Common
+{
+    public static class TypeSwitch
+    {
+        public class CaseInfo
+        {
+
+            public bool IsDefault { get; set; }
+
+            public Type Target { get; set; }
+
+            public Action Action { get; set; }
+
+        }
+
+        public static void Do(Type type, params CaseInfo[] cases)
+        {
+           
+            foreach (var entry in cases)
+            {
+
+                if (entry.IsDefault || type == entry.Target)
+                {
+
+                    entry.Action();
+
+                    break;
+
+                }
+
+            }
+
+        }
+
+
+        public static CaseInfo Case<T>(Action action)
+        {
+
+            return new CaseInfo()
+            {
+
+                Action = action,
+
+                Target = typeof(T)
+
+            };
+
+        }
+
+
+        //public static CaseInfo Case<T>(Action<T> action)
+        //{
+
+        //    return new CaseInfo()
+        //    {
+
+        //        Action = (x) => action((T)x),
+
+        //        Target = typeof(T)
+
+        //    };
+
+        //}
+
+
+        public static CaseInfo Default(Action action)
+        {
+
+            return new CaseInfo()
+            {
+
+                Action = action,
+
+                IsDefault = true
+
+            };
+
+        }
+
+    }
+}
